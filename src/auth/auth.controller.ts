6d0e018836ef3523dfd1e60899c3a6f56139f20a
nokazn <nokazn@gmail.com>
2020-09-14 20:46:25 +0900

    local auth guard

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,12 @@
 import { Controller, Post, Request, UseGuards } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
+import { LocalAuthGuard } from './guard/localAuth.guard';
 
 import { TODO } from 'src/types';
 
 @Controller('auth')
 export class AuthController {
   // passport-local の strategy でガードする
-  @UseGuards(AuthGuard('local'))
+  @UseGuards(LocalAuthGuard)
   @Post('login')
   async login(@Request() req: TODO) {
     return req.user;
